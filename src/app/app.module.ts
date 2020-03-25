fa7db4cea5be2c546dbdab27d3d5b453c83789a1
Daniel Dito <drdito8@gmail.com>
2020-03-24 22:14:24 -0700

    finished initial prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalculatorWidgetComponent } from './calculator-widget/calculator-widget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorWidgetComponent
   ],
   imports: [
     BrowserModule
